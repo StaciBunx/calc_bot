e274d0c6cc550ed0d661d33d4768179fe0a16531
Jeiojey <signofaries@gmail.com>
2022-10-09 21:59:33 +0300

    homework ready

--- log.py
+++ log.py
@@ -1,1 +1,7 @@
 import logging
+
+logging.basicConfig(filename='log.txt',
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO
+                    )
+logger = logging.getLogger(__name__)
